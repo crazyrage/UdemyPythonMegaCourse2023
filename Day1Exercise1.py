997f80bf674664cfabcc6bdb0ac85fd4ae4950d8
Damian <Damian.carter97@gmail.com>
2023-02-27 17:06:31 -0600

    Coursework

---
+++ Day1Exercise1.py
@@ -0,0 +1,5 @@
+#D1 Excercises ~~~~~~~~~~
+
+#Write a Python program that gets input from the user and prints out that input
+name = input ("name?")
+print (name)
--- Day1Main.py
+++ Day1Main.py
@@ -1,13 +1,7 @@
-#Day1~~~
-user_prompt = "Enter a todo:"
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
-
-todos = [todo1, todo2, todo3, "Hello"]
-print(todos)
-
-print(type(todo1))
-
+#D1 Excercises ~~~~~~~~~~
+#1. Create a variable (any name) and store a string in the variable.
+#2. Then, print out the type of the variable.
+v1 = "string"
+print(type(v1))
 
 
